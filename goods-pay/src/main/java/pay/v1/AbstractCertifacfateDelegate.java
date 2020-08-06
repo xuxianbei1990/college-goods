c8b9df864c001db743e72a7a7aa9e447cbe2567e
xuxianbei <34258324+xuxianbei1990@users.noreply.github.com>
2020-08-06 19:03:32 +0800

    支付第二版：流程统一

--- goods-pay/src/main/java/pay/v1/AbstractCertifacfateDelegate.java
+++ goods-pay/src/main/java/pay/v1/AbstractCertifacfateDelegate.java
@@ -5,46 +5,18 @@ import pay.v1.enums.PaymentTypeEnum;
 
 /**
  * @author: xuxianbei
- * Date: 2020/8/1
- * Time: 12:10
+ * Date: 2020/8/5
+ * Time: 21:36
  * Version:V1.0
  */
 public abstract class AbstractCertifacfateDelegate implements Certifacate, UrlCallBackNotify {
 
-    /**
-     * 如何解决 executeNotify  getCertifacate 代码类似问题
-     *
-     * @param value
-     * @return
-     */
-    @Override
-    public Object doCallBackNotify(String value) {
-        //解析分发
-        CertificateEnum certificateEnum = CertificateEnum.Pre_Pay;
-        PaymentTypeEnum paymentTypeEnum = PaymentTypeEnum.ALI_PAY;
+    private final static Integer CERTIFACATE_FUNCTION = 1;
+    private final static Integer CALLBACK_FUNCTION = 2;
 
-        if (certificateEnum.equals(CertificateEnum.Pre_Pay)) {
-            if (support(paymentTypeEnum)) {
-                doPrepayCallBack();
-            }
-        } else if (certificateEnum.equals(CertificateEnum.Call_Third)) {
-            if (support(paymentTypeEnum)) {
-                doCallThirdCallBack();
-            }
-        } else {
-            throw new RuntimeException("不支持");
-        }
+    private Integer function;
 
-        return null;
-    }
-
-    protected void doCallThirdCallBack() {
-    }
-
-
-    protected void doPrepayCallBack() {
-    }
-
+    protected abstract boolean support(PaymentTypeEnum paymentTypeEnum);
 
     /**
      * 派发
@@ -55,29 +27,61 @@ public abstract class AbstractCertifacfateDelegate implements Certifacate, UrlCa
      */
     @Override
     public Object getCertifacate(CertificateEnum certificateEnum, PaymentTypeEnum paymentTypeEnum) {
+        function = CERTIFACATE_FUNCTION;
+        doDispatch(certificateEnum, paymentTypeEnum, function);
+        return null;
+    }
+
+    //第二版
+    private void doDispatch(CertificateEnum certificateEnum, PaymentTypeEnum paymentTypeEnum, Integer function) {
         //未解决问题：如果支付流程发生变更，这里将无法适应
         if (certificateEnum.equals(CertificateEnum.Pre_Pay)) {
             //解决如果有第三种支付厂商出现；
             if (support(paymentTypeEnum)) {
-                doPrePay();
+                if (function == CERTIFACATE_FUNCTION) {
+                    doPrePay();
+                } else if (function == CALLBACK_FUNCTION) {
+                    doPrepayCallBack();
+                }
             }
         } else if (certificateEnum.equals(CertificateEnum.Call_Third)) {
             if (support(paymentTypeEnum)) {
-                doCallThird();
+                if (function == CERTIFACATE_FUNCTION) {
+                    doCallThird();
+                } else if (function == CALLBACK_FUNCTION) {
+                    doCallThirdCallBack();
+                }
             }
         } else {
             throw new RuntimeException("不支持");
         }
+    }
+
+    /**
+     * 如何解决 executeNotify  getCertifacate 代码类似问题
+     *
+     * @param value
+     * @return
+     */
+    @Override
+    public Object doCallBackNotify(String value) {
+        function = CALLBACK_FUNCTION;
+        //解析分发
+        CertificateEnum certificateEnum = CertificateEnum.Pre_Pay;
+        PaymentTypeEnum paymentTypeEnum = PaymentTypeEnum.ALI_PAY;
+        doDispatch(certificateEnum, paymentTypeEnum, function);
         return null;
     }
-
-    protected abstract boolean support(PaymentTypeEnum paymentTypeEnum);
-
-    protected void doPrePay() {
-    }
-
 
     protected void doCallThird() {
     }
 
+    protected void doCallThirdCallBack() {
+    }
+
+    protected void doPrePay() {
+    }
+
+    protected void doPrepayCallBack() {
+    }
 }
